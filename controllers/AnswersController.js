a423377f22c38fad399eef7cc3df280328396f25
Jason Bethel & Austin Blanchard <student@epicodus.com>
2015-09-22 15:50:43 -0700

    add results to results div

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -1,4 +1,6 @@
 surveyData.controller('AnswersCtrl', function AnswersCtrl($scope, SurveyFactory) {
   $scope.answers = SurveyFactory.answers;
+  $scope.shows = SurveyFactory.shows;
+  $scope.destinations = SurveyFactory.destinations;
   $scope.SurveyFactory = SurveyFactory;
 });
